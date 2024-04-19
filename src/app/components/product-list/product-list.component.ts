a2e7a29c69184a075eb6f393cdef6483afa31f5b
AnimateBow4809 <alidashtbozorg709@gmail.com>
2024-04-19 04:21:37 +0330

    first

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent implements OnInit{
+  
+  products:Product[];
+  constructor(private productService:ProductService){}
+  
+  ngOnInit(){
+    this.listProducts();
+    setInterval(() => {
+      this.listProducts();
+    }, 10000);
+  }
+  
+  listProducts(){
+    this.productService.getProductList().subscribe(
+      data=>{
+        this.products=data;
+      }
+    )
+  }
+}
